b8e98c869d6bb6a4b594a9fdcc9a34f8df6e6a71
ardiapri <ardiapri882@gmail.com>
2024-05-21 18:34:20 +0700

    memanggil heder

--- komposisi.cpp
+++ komposisi.cpp
@@ -2,4 +2,13 @@
 #include <string>
 using namespace std;
 
+#include "jantung.h"
+#include "manusia.h"
+
+int main() {
+    manusia* varmanusia = new manusia("jono");
+    delete varmanusia;
+    return 0;
+}
+
 
