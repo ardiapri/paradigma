00be4337f1924d07afa38d84f5df03407cb60cad
ardiapri <ardiapri882@gmail.com>
2024-05-21 20:33:07 +0700

    menambahkan void pasien dan dokter

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -35,3 +35,26 @@ public:
       void cetakpasien();
 };
 
+void pasien::tambahdokter(dokter* pdokter) {
+    daftar_dokter.push_back(pdokter);
+}
+void pasien::cetakdokter() {
+    cout << "daftar dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+void dokter::tambahpasien(pasien* ppasien) {
+    daftar_pasien.push_back(ppasien);
+    ppasien->tambahdokter(this);
+
+}
+void dokter::cetakpasien() {
+    cout << "daftar pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
