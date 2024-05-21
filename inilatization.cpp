f57a7ecf79bfe2494c470ee4a3ec2887a88a2f98
ardiapri <ardiapri882@gmail.com>
2024-05-21 17:41:57 +0700

    public

--- inilatization.cpp
+++ inilatization.cpp
@@ -8,3 +8,17 @@ private:
     string nama;
     float nilai;
 
+public:
+   mahasiswa(int pid, string pnama, float pnilai) :id(pid), nama(pnama), nilai(pnilai){
+
+   }
+      //definisi
+   
+
+   ~mahasiswa() {
+       cout << "id  = " << id << endl;
+       cout << "nama = " << nama << endl;
+       cout << "nilai = " << nilai << endl;
+   }
+};
+
